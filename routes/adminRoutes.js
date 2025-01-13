7000d3e865ab29b3a5d8770524e7bc3c3967fcad
Mishab <85479103+imishab@users.noreply.github.com>
2025-01-13 12:12:34 +0530

    first commit

---
+++ routes/adminRoutes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { signup,
+    signin,
+    signout,
+    getAllUsers,
+    addProduct,
+    getAllProducts,
+    upload,
+    deleteProduct,
+    addCategory,
+    getAllCategories,
+    deleteCategory,
+    getAllOrders,
+    aiImage,
+
+} = require('../controllers/adminController');
+const { protect } = require('../middleware/authMiddleware');
+const router = express.Router();
+
+///////////////AUTH////////////////////////
+router.post('/signup', signup);
+router.post('/signin', signin);
+router.post('/signout', protect, signout);
+router.get('/profile', protect, (req, res) => {
+    res.json(req.admin);
+});
+
+///////////////USERS////////////////////////
+router.get('/users', protect, getAllUsers, (req, res) => {
+    res.json(req.admin);
+});
+
+///////////////PRODUCTS////////////////////////
+router.post('/add-product', protect, upload.single('image'), addProduct);
+router.get('/all-products', protect, getAllProducts);
+router.delete('/delete-product/:id', protect, deleteProduct);
+
+
+///////////////CATEGORY////////////////////////
+router.post('/add-category', protect, addCategory);
+router.get('/all-categories', protect, getAllCategories);
+router.delete('/delete-category/:id', protect, deleteCategory);
+
+///////////////ORDERS////////////////////////
+router.get('/all-orders', protect, getAllOrders);
+
+
+router.post('/ai-image', protect, aiImage);
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const adminRoutes = require('./routes/adminRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:8081'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Database Connection
+connectDB();
+
+// Routes
+app.use('/api/admin', adminRoutes);
+app.use('/api', userRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
