c666906f89f28a51c25571fe737e9247d7465d7f
Mishab <85479103+imishab@users.noreply.github.com>
2025-01-14 23:59:54 +0530

    error fixed on endpoint

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
     'https://admin-tastify.vercel.app',
     'http://192.168.29.75:3000',
     'https://57fzj03p-3000.inc1.devtunnels.ms',
-    'https://frontend-tastify.vercel.app/'
+    'https://frontend-tastify.vercel.app'
 ];
 const corsOptions = {
     origin: (origin, callback) => {
